641deea658802898715bbd3b5d5be9eabc706641
antoine <antoine_arranz@hotmail.com>
2024-05-09 14:04:33 +0200

    ajout du footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,13 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='footer bg-base-100'>
+      <div className='container mx-auto my-10 flex justify-center gap-2 p-2 text-center'>
+        <h2 className='flex flex-col font-bold text-center'>
+          PhantomBuster technical test
+        </h2>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
